e0638a1c1ab5f508fa33fef83f631ba7ee07cd9c
zhanwentao <zhanwentao@baijiahulian.com>
2017-06-30 18:06:20 +0800

    role info

--- mock/GET/role/info.js
+++ mock/GET/role/info.js
@@ -20,26 +20,10 @@ exports.index = function (param) {
         appId: 6,
         tag: "yunying_tianyan_jingli",
         status: 0,
-        openRoleUid: "xxx",
-        parentAccountId: "xxx",
-        parentAccountRoleId: "xxx",
-        parentOpenRoleUid: "xxx",
-        createTime: new Date().getTime(),
         hasPermissions: util.getPermissions()
     };
 
-    result.data = {
-        id: 'xxx',// ID
-        appId: 6, // 业务系统
-        title: "产品经理", // 职位
-        department: "xxx", // ehr岗位信息
-        username: "dudu", // 帐号
-        displayName: "李阳", // 姓名
-        manager: "xxx", // 汇报上级
-        createTime: new Date().getTime(), // 帐号开通时间
-        status: 0, // 状态
-        currentRole: currentRole
-    }
+    result.data = currentRole;
 
     return result;
 }
