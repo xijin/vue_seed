85a026c72d2dd147653b86002b18be258d61994d
zhanwentao <zhanwentao@baijiahulian.com>
2017-07-10 11:00:24 +0800

    ac get auth

--- src/commonRequest.js
+++ src/commonRequest.js
@@ -4,7 +4,7 @@ import ajaxService from "./common/utils/ajaxService"
 export default {
     loginIn: function (params) {
         var params = params || {};
-        return ajaxService.send('/manage/getAuth.json', {data: params});
+        return ajaxService.send('/ac/getAuth.json', {data: params});
     },
     getSelectInfo: function (params) {
         var params = params || {};
